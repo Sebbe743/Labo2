51307bd3d7a39b97232050167c12773c315bd99a
Sebbe743 <79916322+Sebbe743@users.noreply.github.com>
2022-10-10 10:56:51 +0200

    Update Helloworld.cpp

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 #include <cstdlib>
 
-
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-
     std::cout<<"hello world"<< std::endl;
     system("pause");
 
